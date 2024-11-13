e38f3b4e75ec0fcdc65297a07d3aac978f81b894
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-11-13 07:53:17 -0300

    Adiciona logica de cooldown em interacao de evento

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, Collection } = require('discord.js');
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -10,6 +10,11 @@ module.exports = {
 
     if (!command) {
       console.error(`No command matching ${interaction.commandName} was found.`);
+    }
+
+    const cooldown = getCooldown(interaction, command);
+    if (cooldown !== null) {
+      return cooldown;
     }
 
     try {
@@ -25,4 +30,34 @@ module.exports = {
       }
     }
   },
+};
+
+const getCooldown = (interaction, command) => {
+  const { cooldowns } = interaction.client;
+
+  if (!cooldowns.has(command.data.name)) {
+    cooldowns.set(command.data.name, new Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.data.name);
+  const defaultCooldownDuration = 3;
+  const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+  if (timestamps.has(interaction.user.id)) {
+    const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const expiredTimeStamp = Math.round(expirationTime / 1_000);
+      return interaction
+        .reply({
+          content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use this command again in <t:${expiredTimeStamp}:R>`, ephemeral: true,
+        });
+    }
+  }
+
+  timestamps.set(interaction.user.id, now);
+  setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+  return null;
 };
